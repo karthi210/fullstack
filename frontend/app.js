8256897d59cd735d5f9d6ea5fb3c481f91bd4ffb
karthi210 <119928109+karthi210@users.noreply.github.com>
2026-02-09 13:34:47 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:8000/api/login", {
+  const res = await fetch("http://localhost:8002/api/login", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({ email, password })
