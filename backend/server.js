4792a540970e99a9ff75ba6a680520b82c742082
karthi210 <119928109+karthi210@users.noreply.github.com>
2026-03-26 15:39:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,6 @@ app.get('/metrics', async (req, res) => {
     res.end(await register.metrics());
 });
 
-app.listen(8000, () => {
-    console.log("Server running on port 8000");
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running");
 });
